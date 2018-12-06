3b9cab663be4d7e2fd0bac7259c5e9b8d15b48d7
burin <21989631+burinm@users.noreply.github.com>
2018-12-05 22:02:05 -0700

    efr32bg1b232f256gm48 project, no external LFXO, no DCDC converter used

---
+++ maketags.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SDK_ROOT=~/SimplicityStudio_v4/developer/sdks/gecko_sdk_suite/v2.4
+
+EM_LIB=${SDK_ROOT}/platform/emlib
+PLATFORM=${SDK_ROOT}/platform/Device/SiliconLabs/EFR32BG1B
+BLE_LIB=${SDK_ROOT}/protocol/bluetooth/lib/EFR32BG1B
+
+find -iname "*.h" -or -iname "*.c" -or -iname "*.cpp" -or -iname "*.s" -or -name Makefile > cscope.files
+
+find ${EM_LIB} -iname "*.h" -or -iname "*.c" -or -iname "*.cpp" -or -iname "*.s" -or -name Makefile >> cscope.files
+find ${PLATFORM} -iname "*.h" -or -iname "*.c" -or -iname "*.cpp" -or -iname "*.s" -or -name Makefile >> cscope.files
+find ${BLE_LIB} -iname "*.h" -or -iname "*.c" -or -iname "*.cpp" -or -iname "*.s" -or -name Makefile >> cscope.files
+
+cscope -b
+/home/burin/ctags-u/bin/ctags -L ./cscope.files
+cscope
